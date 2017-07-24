3038e74762be06f71f374242104e580ddf126d8c
William Hill <hill119@llnl.gov>
2017-07-24 14:57:50 -0700

    added esg-autoinstall and esg-autoinstall.template to esgf-installer component list

--- create_esgf_tarballs.sh
+++ create_esgf_tarballs.sh
@@ -22,7 +22,7 @@ declare -A components
 components[esgf-dashboard]='bin/esg-dashboard dist/esgf_dashboard-0.0.2-py2.7.egg INSTALL README LICENSE'
 components[esgf-desktop]='bin/esg-desktop INSTALL README LICENSE'
 components[esgf-idp]='bin/esg-idp INSTALL README LICENSE'
-components[esgf-installer]='jar_security_scan setup-autoinstall globus/esg-globus esg-bootstrap esg-node esg-init esg-functions esg-gitstrap esg-node.completion esg-purge.sh esg-autoinstall-testnode compute-tools/esg-compute-languages compute-tools/esg-compute-tools INSTALL README LICENSE'
+components[esgf-installer]='jar_security_scan setup-autoinstall globus/esg-globus esg-bootstrap esg-node esg-init esg-functions esg-gitstrap esg-node.completion esg-purge.sh esg-autoinstall-testnode esg-autoinstall esg-autoinstall.template compute-tools/esg-compute-languages compute-tools/esg-compute-tools INSTALL README LICENSE'
 components[esgf-node-manager]='bin/esg-node-manager bin/esgf-sh bin/esgf-spotcheck etc/xsd/registration/registration.xsd INSTALL README LICENSE'
 components[esgf-security]='bin/esgf-user-migrate bin/esg-security bin/esgf-policy-check INSTALL README LICENSE'
 #components[esgf-web-fe]='bin/esg-web-fe INSTALL README LICENSE'
@@ -30,7 +30,7 @@ components[esg-orp]='bin/esg-orp INSTALL README LICENSE'
 components[esgf-getcert]='INSTALL README LICENSE'
 components[esg-search]='bin/esg-search bin/esgf-crawl bin/esgf-optimize-index etc/conf/jetty/jetty.xml-auth etc/conf/jetty/realm.properties etc/conf/solr/schema.xml etc/conf/solr/solrconfig.xml etc/conf/solr/solrconfig.xml-replica etc/conf/solr/solr.xml-master etc/conf/solr/solr.xml-slave etc/conf/jetty/webdefault.xml-auth INSTALL README LICENSE'
 components[esgf-product-server]='esg-product-server'
-components[filters]='esg-access-logging-filter esg-drs-resolving-filter esg-security-las-ip-filter esg-security-tokenless-filters' 
+components[filters]='esg-access-logging-filter esg-drs-resolving-filter esg-security-las-ip-filter esg-security-tokenless-filters'
 components[esgf-cog]='esg-cog'
 components[esgf-stats-api]='bin/esg_stats-api_v2 dist/esgf-stats-api.war'
 rm -rf esgf_tarballs
@@ -53,17 +53,17 @@ for i in "${!components[@]}"; do
 	for file in ${components[$i]}; do
 		if [ ! -e $i/$file ]; then
 			echo "File $i/$file not found";
-			continue;	
-		else  
+			continue;
+		else
 			echo "File $i/$file OK";
 			cp $i/$file temp-dists
 		fi
 	done
 	cd temp-dists;
-	for f in `ls`; do 
-		if echo $f|grep md5 >/dev/null; then 
-			continue; 
-		else 
+	for f in `ls`; do
+		if echo $f|grep md5 >/dev/null; then
+			continue;
+		else
 			if [ "$f" = "esg-node" ]; then
 				sed -i .backup "s/\(script_version=\"$quotedsv\"\)/script_version=\"$script_version\"/" esg-node;
 				sed -i .backup "s/\(script_release=\"$quotedsr\"\)/script_release=\"$script_release\"/" esg-node;
@@ -72,7 +72,7 @@ for i in "${!components[@]}"; do
 			if [ "$f" = "esg-bootstrap" ]; then
 				sed -i .backup "s/\(script_maj_version=\"$quotedmj\"\)/script_maj_version=\"$script_maj_version\"/" esg-bootstrap;
 			fi
-			md5sum $f >$f.md5; 
+			md5sum $f >$f.md5;
 		fi
 	 done
 	if [ "$i" = "esgf-installer" ]; then
