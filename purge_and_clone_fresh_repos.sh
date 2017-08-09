a4f97f4170709acd67f1c27bb8df45b732bcba49
William Hill <hill119@llnl.gov>
2017-08-09 15:51:41 -0700

    added esgf-stats-api to purge script

--- purge_and_clone_fresh_repos.sh
+++ purge_and_clone_fresh_repos.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #Deletes all existing repos and clones new copies
 
-rm -rf esgf-dashboard esgf-desktop esgf-getcert esgf-idp esgf-installer esgf-node-manager esgf-publisher-resources esgf-security esgf-web-fe esg-orp esg-publisher esg-search
+rm -rf esgf-dashboard esgf-desktop esgf-getcert esgf-idp esgf-installer esgf-node-manager esgf-publisher-resources esgf-security esgf-web-fe esg-orp esg-publisher esg-search esgf-stats-api
 
-while read ln; do 
+while read ln; do
 	git clone $ln;
 done <allrepos.txt
