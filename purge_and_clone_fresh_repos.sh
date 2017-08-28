f833eb430d267e1e5b6199fc2952bff4311a9cad
William Hill <mjh2225@gmail.com>
2017-08-28 13:32:23 -0700

    added changing directories

--- purge_and_clone_fresh_repos.sh
+++ purge_and_clone_fresh_repos.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 #Deletes all existing repos and clones new copies
 
-rm -rf esgf-dashboard esgf-desktop esgf-getcert esgf-idp esgf-installer esgf-node-manager esgf-publisher-resources esgf-security esgf-web-fe esg-orp esg-publisher esg-search esgf-stats-api
+rm -rf ../esgf-dashboard ../esgf-desktop ../esgf-getcert ../esgf-idp ../esgf-node-manager ../esgf-publisher-resources ../esgf-security ../esgf-web-fe ../esg-orp ../esg-publisher ../esg-search ../esgf-stats-api
 
 while read ln; do
-	git clone $ln;
+	pushd ../
+		git clone $ln;
+	popd
 done <allrepos.txt
