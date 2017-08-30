e439d9ec2804a8f7ac94a84bc63ac815a75bcff5
William Hill <hill119@llnl.gov>
2017-08-29 17:21:42 -0700

    extracted clone and purge into separate functions; added option for user to define repo directory

--- purge_and_clone_fresh_repos.py
+++ purge_and_clone_fresh_repos.py
@@ -1,34 +1,51 @@
 #!usr/bin/env python
-import shutil
-import repo_info
-from git import Repo
 import os
 import re
-
-#TODO: do not pass esgf-installer when not on personal computer
-
-#Search for and remove appropriate repos
-for repo in repo_info.REPO_LIST:
-    if repo == 'esgf-installer':
-        continue
-    try:
-        shutil.rmtree('/Users/brooks47/development/' + repo)
-        print(repo + " removed succesfully.")
-    except:
-        print(repo + " does not exist on this system.")
-        print( repo + " skipped.")
+import shutil
+from git import Repo
+from git import RemoteProgress
+import repo_info
 
 
-for repo_url in repo_info.ALL_REPO_URLS:
-    #TODO: to make universal, allow user to enter the directory path to clone to
+class MyProgressPrinter(RemoteProgress):
+    def update(self, op_code, cur_count, max_count=None, message=''):
+        if message:
+            print self._cur_line
+            print message
 
-    ###In the future, if a module with more than 2 "-"s is created, this will
-    ###need to be revised
-    #strips.git from the string to make using reg-ex easier
-    strip_repo = repo_url.replace(".git", "")
-    directory_name = re.search('esg\w*-\w+\W*\w+', strip_repo).group()
-    if directory_name == 'esgf-installer':
-        continue
-    print("Currently working on directory: " + directory_name)
-    Repo.clone_from(repo_url, '/Users/brooks47/development/' + directory_name)
-    print(directory_name + " succesfully cloned.")
+
+def purge_repos(repo_directory):
+    for repo in repo_info.REPO_LIST:
+        try:
+            shutil.rmtree(os.path.join(repo_directory, repo))
+            print repo + " removed succesfully."
+        except OSError:
+            print repo + " does not exist on this system."
+            print(repo + " skipped.")
+
+
+def clone_repos(repo_directory):
+    for repo_url in repo_info.ALL_REPO_URLS:
+        # In the future, if a module with more than 2 "-"s is created, this will
+        # need to be revised
+        # strips.git from the string to make using reg-ex easier
+        strip_repo = repo_url.replace(".git", "")
+        directory_name = re.search('esg\w*-\w+\W*\w+', strip_repo).group()
+        repo_path = os.path.join(repo_directory, directory_name)
+        print("Currently cloning repo: " + directory_name)
+        Repo.clone_from(repo_url, repo_path,
+                        progress=MyProgressPrinter())
+        print(directory_name + " successfully cloned -> {repo_path}".format(repo_path=repo_path))
+
+
+def main():
+    # Search for and remove appropriate repos
+    default_repo_directory = os.path.join(os.environ["HOME"], "Development")
+    repo_directory = raw_input("Enter the directory where the repos will be placed [{default_repo_directory}]: ".format(
+        default_repo_directory=default_repo_directory)) or default_repo_directory
+    purge_repos(repo_directory)
+    clone_repos(repo_directory)
+
+
+if __name__ == '__main__':
+    main()
