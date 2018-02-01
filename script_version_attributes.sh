cecfe80a42e53c15a1b52a8d6653feb852c56320
Prashanth Dwarakanath <pchengi@nsc.liu.se>
2018-02-01 15:31:42 +0100

    2.6.1 devel ready

--- script_version_attributes.sh
+++ script_version_attributes.sh
@@ -1,5 +1,5 @@
 script_maj_version=2.6
-script_sub_version=0
+script_sub_version=1
 release_type="devel" #devel/master
 script_version="v${script_maj_version}.${script_sub_version}-${release_type}-release"
 script_release="Asgard"
