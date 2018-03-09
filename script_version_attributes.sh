ba049e054fca103f89f456b264c4fda39b385d5d
Prashanth Dwarakanath <pchengi@nsc.liu.se>
2018-03-09 15:44:26 +0100

    update script version for 2.6.6 master release

--- script_version_attributes.sh
+++ script_version_attributes.sh
@@ -1,5 +1,5 @@
 script_maj_version=2.6
-script_sub_version=5
+script_sub_version=6
 release_type="master" #devel/master
 script_version="v${script_maj_version}.${script_sub_version}-${release_type}-release"
 script_release="Asgard"
