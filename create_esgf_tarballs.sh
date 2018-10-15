1d11834cd06c6314357ba1c190dd0de5634617af
Prashanth Dwarakanath <pchengi@nsc.liu.se>
2018-10-15 18:51:26 +0200

    add tds-extrajars.txt to filters components

--- create_esgf_tarballs.sh
+++ create_esgf_tarballs.sh
@@ -42,7 +42,7 @@ components[esg-orp]='bin/esg-orp INSTALL README LICENSE etc/conf/esg-orp.propert
 components[esgf-getcert]='INSTALL README LICENSE'
 components[esg-search]='bin/esg-search bin/esgf-crawl bin/esgf-optimize-index etc/conf/jetty/jetty.xml-auth etc/conf/jetty/realm.properties etc/conf/solr/schema.xml etc/conf/solr/solrconfig.xml etc/conf/solr/solrconfig.xml-replica etc/conf/solr/solr.xml-master etc/conf/solr/solr.xml-slave etc/conf/jetty/webdefault.xml-auth INSTALL README LICENSE solr-home.tar etc/conf/solr/log4j.properties'
 components[esgf-product-server]='esg-product-server'
-components[filters]='esg-access-logging-filter esg-drs-resolving-filter esg-security-las-ip-filter esg-security-tokenless-filters commons-httpclient-3.1.jar commons-lang-2.6.jar esg-security-tokenless-thredds-filters.xml jdom-legacy-1.1.3.jar esg-security-las-ip-filter.xml esapi-2.1.0.1.jar joda-time-2.2.jar not-yet-commons-ssl-0.3.9.jar opensaml-2.6.6.jar openws-1.5.6.jar slf4j-api-1.7.6.jar xmlsec-1.5.7.jar xmltooling-1.4.4.jar'
+components[filters]='esg-access-logging-filter esg-drs-resolving-filter esg-security-las-ip-filter esg-security-tokenless-filters commons-httpclient-3.1.jar commons-lang-2.6.jar esg-security-tokenless-thredds-filters.xml jdom-legacy-1.1.3.jar esg-security-las-ip-filter.xml esapi-2.1.0.1.jar joda-time-2.2.jar not-yet-commons-ssl-0.3.9.jar opensaml-2.6.6.jar openws-1.5.6.jar slf4j-api-1.7.6.jar xmlsec-1.5.7.jar xmltooling-1.4.4.jar tds-extrajars.txt'
 components[esgf-cog]='esg-cog'
 components[esgf-stats-api]='bin/esg_stats-api_v2 dist/esgf-stats-api.war'
 
