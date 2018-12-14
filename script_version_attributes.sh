5afc95cbe217787bdf6c1beac8aeb4f7cfc3a020
Prashanth Dwarakanath <pchengi@nsc.liu.se>
2018-12-14 12:07:57 +0100

    update script version

--- script_version_attributes.sh
+++ script_version_attributes.sh
@@ -1,5 +1,5 @@
 script_maj_version=2.8
-script_sub_version=0
-release_type="devel" #devel/master
+script_sub_version=RC2
+release_type="master" #devel/master
 script_version="v${script_maj_version}.${script_sub_version}-${release_type}-release"
 script_release="Winterfell"
