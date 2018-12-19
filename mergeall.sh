50f8b3ea0265f98e034c73c7cfbb4d84d45ba6bc
Prashanth Dwarakanath <pchengi@nsc.liu.se>
2018-12-19 18:17:16 +0100

    update release version

--- mergeall.sh
+++ mergeall.sh
@@ -5,7 +5,7 @@ while read ln; do
     repo=`echo $ln|cut -d':' -f1`;
     tag=`echo $ln|cut -d':' -f2`;
     cd $repo
-    git checkout master
+    git checkout master && git pull
     git merge $tag
     git push
     nt=`git describe`
