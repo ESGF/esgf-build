3aa52f4d16b900b2c86b2b803c77fb22720b2c44
William Hill <hill119@llnl.gov>
2019-01-03 15:20:17 -0800

    fixed flake8 errors

--- repo_info.py
+++ repo_info.py
@@ -1,13 +1,4 @@
-'''    '''
-# SCRIPT DEFAULT INFO
-# SCRIPT_MAJOR_VERSION = '2.5'
-# SCRIPT_RELEASE = 'Midgard'
-# SCRIPT_VERSION = 'v2.5.8-master-release'
-
-SCRIPT_INFO = {'script_major_version': '2.5',
-               'script_release': 'Helheim',
-               'script_version': 'v2.5.13-master-release'}
-
+"""File for repository information constants."""
 REPOS_TO_EXCLUDE = ['esgf-installer', 'esgf-publisher-resources', 'esg-publisher', 'esgf-desktop']
 
 ALL_REPO_URLS = {"esgf-dashboard": 'https://github.com/ESGF/esgf-dashboard.git',
@@ -34,30 +25,17 @@ REPO_LIST = [
     'esgf-stats-api'
 ]
 ########################################################################
-#last minute solution for "esgf-installer" does not exist problem
-#this list creates all local mirrors and tarballs, but does not build all
-CREATE_DIRECTORY_LIST = [
-    'esgf-dashboard',
-    'esgf-getcert',
-    'esgf-idp',
-    'esgf-installer',
-    'esgf-node-manager',
-    'esgf-security',
-    'esg-orp',
-    'esg-search',
-    'esgf-stats-api'
-]
 #########################################################################
 REPO_MENU = 'Repository menu:\n'\
-'----------------------------------------\n'\
-'0: esgf-dashboard\n'\
-'1: esgf-getcert\n'\
-'2: esgf-idp\n'\
-'3: esgf-node-manager\n'\
-'4: esgf-security\n'\
-'5: esg-orp\n'\
-'6: esg-search\n'\
-'7: esgf-stats-api\n'\
-"To select a repo, enter the appropriate number.\n"\
-"To select multiple repos, seperate each number with a comma.\n"\
-"Example: '0, 3, 5'\n"
+    '----------------------------------------\n'\
+    '0: esgf-dashboard\n'\
+    '1: esgf-getcert\n'\
+    '2: esgf-idp\n'\
+    '3: esgf-node-manager\n'\
+    '4: esgf-security\n'\
+    '5: esg-orp\n'\
+    '6: esg-search\n'\
+    '7: esgf-stats-api\n'\
+    "To select a repo, enter the appropriate number.\n"\
+    "To select multiple repos, seperate each number with a comma.\n"\
+    "Example: '0, 3, 5'\n"
