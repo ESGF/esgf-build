506ae5b16f83de843cb8c80f459a01c4afe5d266
Prashanth Dwarakanath <pchengi@nsc.liu.se>
2019-02-14 17:48:43 +0100

    update script version to 2.8.4

--- script_version_attributes.sh
+++ script_version_attributes.sh
@@ -1,5 +1,5 @@
 script_maj_version=2.8
-script_sub_version=1
+script_sub_version=4
 release_type="master" #devel/master
 script_version="v${script_maj_version}.${script_sub_version}-${release_type}-release"
 script_release="Winterfell"
